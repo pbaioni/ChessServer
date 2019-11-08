451e8c0f7174e0d0a977b3617783f5f667308375
Paolo <baioni2002@yahoo.fr>
2019-11-08 17:42:37 +0100

    implement show pawn structure

--- src/main/java/app/main/service/BoardService.java
+++ src/main/java/app/main/service/BoardService.java
@@ -8,9 +8,63 @@ import org.springframework.stereotype.Service;
 public class BoardService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(BoardService.class);
-	
+
 	public String getOnlyPawnsFEN(String fen) {
-		return "8/pppppppp/8/8/8/8/PPPPPPPP/8 w KQkq - 0 1";
+		LOGGER.info("cleaninf fen: " + fen);
+		String[] splitFen = fen.split("/");
+		String separator = "/";
+		String cleanRows = "";
+		for (int i = 0; i < 8; i++) {
+			String fenRowToProcess;
+			if (i == 7) {
+				fenRowToProcess = replaceNumbers(splitFen[i].split(" ")[0]);
+				separator = "";
+			} else {
+				fenRowToProcess = replaceNumbers(splitFen[i]);
+			}
+
+			LOGGER.info("fen raw row: " + fenRowToProcess);
+			String cleanFenRow = "";
+			for (int j = 0; j < fenRowToProcess.length(); j++) {
+				if (!fenRowToProcess.substring(j, j + 1).equals("p")
+						&& !fenRowToProcess.substring(j, j + 1).equals("P")) {
+					cleanFenRow += " ";
+				} else {
+					cleanFenRow += fenRowToProcess.substring(j, j + 1);
+				}
+			}
+			cleanFenRow = replaceBlanks(cleanFenRow);
+			cleanRows += cleanFenRow + separator;
+		}
+		return "{\"fenOnlyPawns\": \"" + cleanRows + " w KQkq - 0 1\"}";
 	}
-	
+
+	private String replaceNumbers(String fenRowWithNumbers) {
+
+		fenRowWithNumbers = fenRowWithNumbers.replace("1", " ");
+		fenRowWithNumbers = fenRowWithNumbers.replace("2", "  ");
+		fenRowWithNumbers = fenRowWithNumbers.replace("3", "   ");
+		fenRowWithNumbers = fenRowWithNumbers.replace("4", "    ");
+		fenRowWithNumbers = fenRowWithNumbers.replace("5", "     ");
+		fenRowWithNumbers = fenRowWithNumbers.replace("6", "      ");
+		fenRowWithNumbers = fenRowWithNumbers.replace("7", "       ");
+		fenRowWithNumbers = fenRowWithNumbers.replace("8", "        ");
+
+		return fenRowWithNumbers;
+	}
+
+	private String replaceBlanks(String fenRowWithBlanks) {
+
+		fenRowWithBlanks = fenRowWithBlanks.replace("        ", "8");
+		fenRowWithBlanks = fenRowWithBlanks.replace("       ", "7");
+		fenRowWithBlanks = fenRowWithBlanks.replace("      ", "6");
+		fenRowWithBlanks = fenRowWithBlanks.replace("     ", "5");
+		fenRowWithBlanks = fenRowWithBlanks.replace("    ", "4");
+		fenRowWithBlanks = fenRowWithBlanks.replace("   ", "3");
+		fenRowWithBlanks = fenRowWithBlanks.replace("  ", "2");
+		fenRowWithBlanks = fenRowWithBlanks.replace(" ", "1");
+
+		return fenRowWithBlanks;
+	}
+
 }
