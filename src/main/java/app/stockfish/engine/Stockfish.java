2196eb35e9d8bfa1a03e9fd754a2966a65b28d2c
Paolo <baioni2002@yahoo.fr>
2019-11-09 23:13:13 +0100

    better evaluation extraction

--- src/main/java/app/stockfish/engine/Stockfish.java
+++ src/main/java/app/stockfish/engine/Stockfish.java
@@ -1,7 +1,5 @@
 package app.stockfish.engine;
 
-
-
 import java.io.IOException;
 import java.util.List;
 
@@ -12,93 +10,87 @@ import app.stockfish.engine.enums.Variant;
 import app.stockfish.exceptions.StockfishInitException;
 
 public class Stockfish extends UCIEngine {
-    public Stockfish(String path, Variant variant, Option... options) throws StockfishInitException {
-        super(path, variant, options);
-    }
+	public Stockfish(String path, Variant variant, Option... options) throws StockfishInitException {
+		super(path, variant, options);
+	}
 
-    public String makeMove(Query query) {
-        waitForReady();
-        sendCommand("position fen " + query.getFen() + " moves " + query.getMove());
-        return getFen();
-    }
+	public String makeMove(Query query) {
+		waitForReady();
+		sendCommand("position fen " + query.getFen() + " moves " + query.getMove());
+		return getFen();
+	}
 
-    
 	public String getEngineEvaluation(Query query) {
 		String evaluation = "";
 		String bestMove = "";
 		String separator = " ";
-        waitForReady();
-        sendCommand("position fen " + query.getFen());
+		waitForReady();
+		sendCommand("position fen " + query.getFen());
 
-        StringBuilder command = new StringBuilder("go ");
+		StringBuilder command = new StringBuilder("go ");
 
-        if (query.getDepth() >= 0)
-            command.append("depth ").append(query.getDepth()).append(" ");
+		if (query.getDepth() >= 0)
+			command.append("depth ").append(query.getDepth()).append(" ");
 
-        if (query.getMovetime() >= 0)
-            command.append("movetime ").append(query.getMovetime());
+		if (query.getMovetime() >= 0)
+			command.append("movetime ").append(query.getMovetime());
 
-        waitForReady();
-        sendCommand(command.toString());
-        List<String> response = readResponse("bestmove");
-        for(String line : response) {
-        	if(line.startsWith("info depth 25 seldepth")) {
-        		evaluation = line.split("score cp")[1].trim().split(" ")[0];
-        	}
-        	if(line.startsWith("bestmove")) {
-        		bestMove = line.substring(9).split("\\s+")[0];
-        	}
-        }
-        String absoluteEvaluation = calculateAbsoluteEvaluation(FenHelper.getTurn(query.getFen()), evaluation);
-        String rval = (absoluteEvaluation + separator + bestMove).trim();
-        System.out.println("Engine evaluation: " + rval);
-        return rval;
+		waitForReady();
+		sendCommand(command.toString());
+		List<String> response = readResponse("bestmove");
+
+		evaluation = response.get(response.size() - 2).split("score cp")[1].trim().split(" ")[0];
+		bestMove = response.get(response.size() - 1).substring(9).split("\\s+")[0];
+
+	String absoluteEvaluation = calculateAbsoluteEvaluation(FenHelper.getTurn(query.getFen()), evaluation);
+	String rval = (absoluteEvaluation + separator + bestMove)
+			.trim();System.out.println("Engine evaluation: "+rval);return rval;
 	}
 
-    public String getLegalMoves(Query query) {
-        waitForReady();
-        sendCommand("position fen " + query.getFen());
+	public String getLegalMoves(Query query) {
+		waitForReady();
+		sendCommand("position fen " + query.getFen());
 
-        waitForReady();
-        sendCommand("go perft 1");
+		waitForReady();
+		sendCommand("go perft 1");
 
-        StringBuilder legal = new StringBuilder();
-        List<String> response = readResponse("Nodes");
+		StringBuilder legal = new StringBuilder();
+		List<String> response = readResponse("Nodes");
 
-        for (String line : response)
-            if (!line.isEmpty() && !line.contains("Nodes") && line.contains(":"))
-                legal.append(line.split(":")[0]).append(" ");
+		for (String line : response)
+			if (!line.isEmpty() && !line.contains("Nodes") && line.contains(":"))
+				legal.append(line.split(":")[0]).append(" ");
 
-        return legal.toString();
-    }
+		return legal.toString();
+	}
 
-    public void close() throws IOException {
-        try {
-            sendCommand("quit");
-        } finally {
-            process.destroy();
-            input.close();
-            output.close();
-        }
-    }
-
-    private String getFen() {
-        waitForReady();
-        sendCommand("d");
-
-        return readLine("Fen: ").substring(5);
-    }
-    
-	private String calculateAbsoluteEvaluation(String turn, String cpEval) {
-		
-		Double doubleEval  = Double.parseDouble(cpEval);
-		
-		if(turn.equals("b")) {
-			doubleEval = doubleEval*(-1);
+	public void close() throws IOException {
+		try {
+			sendCommand("quit");
+		} finally {
+			process.destroy();
+			input.close();
+			output.close();
 		}
-		
-		doubleEval = doubleEval/100;
-		
+	}
+
+	private String getFen() {
+		waitForReady();
+		sendCommand("d");
+
+		return readLine("Fen: ").substring(5);
+	}
+
+	private String calculateAbsoluteEvaluation(String turn, String cpEval) {
+
+		Double doubleEval = Double.parseDouble(cpEval);
+
+		if (turn.equals("b")) {
+			doubleEval = doubleEval * (-1);
+		}
+
+		doubleEval = doubleEval / 100;
+
 		return Double.toString(doubleEval);
 	}
 }
