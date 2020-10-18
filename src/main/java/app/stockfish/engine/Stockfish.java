319f5285384fe550c8e4682656441f2a6dcca1b1
Paolo Baioni <baioni2002@yahoo.fr>
2020-10-18 12:05:22 +0200

    no ucinewgame at each command

--- src/main/java/app/stockfish/engine/Stockfish.java
+++ src/main/java/app/stockfish/engine/Stockfish.java
@@ -10,25 +10,28 @@ import app.stockfish.engine.enums.Variant;
 import app.stockfish.exceptions.StockfishInitException;
 
 public class Stockfish extends UCIEngine {
-	
+
 	boolean stop = false;
-	
+
 	public Stockfish(String path, Variant variant, Boolean ownBook, Option... options) throws StockfishInitException {
 		super(path, variant, ownBook, options);
 	}
 
-	public String makeMove(Query query) {
+	public String getFenAfterMove(Query query) {
+		
 		waitForReady();
 		sendCommand("position fen " + query.getFen() + " moves " + query.getMove());
+		
 		return getFen();
 	}
 
 	public String getEngineEvaluation(Query query) {
 
-		waitForReady();
-		sendCommand("ucinewgame");
+		// waitForReady();
+		// sendCommand("ucinewgame");
 		waitForReady();
 		sendCommand("position fen " + query.getFen());
+		waitForReady();
 
 		StringBuilder command = new StringBuilder("go ");
 
@@ -38,12 +41,10 @@ public class Stockfish extends UCIEngine {
 		if (query.getMovetime() >= 0)
 			command.append("movetime ").append(query.getMovetime());
 
-		waitForReady();
 		sendCommand(command.toString());
-		
-		String evaluation = readEvaluation(FenHelper.getTurn(query.getFen()));
+		String evaluation = getAbsoluteEvaluation(FenHelper.getTurn(query.getFen()));
 
-		if(stop) {
+		if (stop) {
 			evaluation += " canceled";
 			stop = false;
 		}
@@ -58,14 +59,27 @@ public class Stockfish extends UCIEngine {
 		waitForReady();
 		sendCommand("go perft 1");
 
-		StringBuilder legal = new StringBuilder();
-		List<String> response = readResponse("Nodes");
+		List<String> response = readLines("Nodes");
 
+		StringBuilder legal = new StringBuilder();
 		for (String line : response)
 			if (!line.isEmpty() && !line.contains("Nodes") && line.contains(":"))
 				legal.append(line.split(":")[0]).append(" ");
 
 		return legal.toString();
+	}
+
+	private String getFen() {
+		waitForReady();
+		sendCommand("d");
+
+		return readLine("Fen: ").substring(5);
+	}
+
+	public void cancel() throws IOException {
+
+		stop = true;
+		sendCommand("stop");
 	}
 
 	public void close() throws IOException {
@@ -77,18 +91,4 @@ public class Stockfish extends UCIEngine {
 			output.close();
 		}
 	}
-	
-	public void cancel() throws IOException {
-		
-		stop = true;
-		sendCommand("stop");
-	}
-
-	private String getFen() {
-		waitForReady();
-		sendCommand("d");
-
-		return readLine("Fen: ").substring(5);
-	}
-
 }
